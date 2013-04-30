8b3a7d16deedc40b810a71345a5082530c99615d
middric <richmiddleditch@gmail.com>
2013-04-30 22:13:36 +0100

    switching to expressjs

---
+++ app.js
@@ -0,0 +1,42 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes')
+  , http = require('http')
+  , path = require('path')
+  , sass = require('node-sass');
+
+var app = express();
+
+app.configure(function () {
+	// all environments
+	app.set('port', process.env.PORT || 3000);
+	app.set('views', __dirname + '/views');
+	app.set('view engine', 'ejs');
+	app.use(sass.middleware({
+		src: __dirname + '/public/sass',
+		dest: __dirname + '/public',
+		debug: true,
+		outputStyle: 'compressed'
+	}));
+	app.use(express.favicon());
+	app.use(express.logger('dev'));
+	app.use(express.bodyParser());
+	app.use(express.methodOverride());
+	app.use(app.router);
+	app.use(express.static(path.join(__dirname, 'public')));
+})
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+app.get('/', routes.index);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
