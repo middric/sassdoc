f5373e621664982d386af8d894e12437d8062a81
middric <richmiddleditch@gmail.com>
2013-05-01 21:06:40 +0100

    adding bootstrap config set spec

--- spec/bootstrap.spec.js
+++ spec/bootstrap.spec.js
@@ -9,16 +9,25 @@ describe('Bootstrap', function () {
 	});
 
 	it("should throw an error with a missing config file", function () {
-		var filename = 'missingfile';
+		var filename = 'missingconfig.json';
 		expect(function () {
 			Bootstrap([1, 2, filename]);
 		}).toThrow(new e.FileDoesNotExist(filename));
 	});
 
 	it("should throw an error with an invalid config file", function () {
-		var filename = 'spec/fixtures/invalidfile.json';
+		var filename = 'spec/fixtures/invalidconfig.json';
 		expect(function () {
 			Bootstrap([1, 2, filename]);
 		}).toThrow(new e.InvalidJSONFile(filename));
 	});
+
+	it("should set an app variable containing the config json", function () {
+		var filename = 'spec/fixtures/validconfig.json',
+			app = require('express')(),
+			json = require('./fixtures/validconfig.json');
+
+		Bootstrap([1, 2, filename], app);
+		expect(app.get('configuration')).toEqual(json);
+	})
 });
