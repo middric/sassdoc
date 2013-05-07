2da75c489a945ed638ef5b924d1b6b3bfcc69904
middric <richmiddleditch@gmail.com>
2013-05-07 09:24:47 +0100

    multiple file support added

--- config/routes.js
+++ config/routes.js
@@ -12,7 +12,7 @@ module.exports = function (app) {
 		var files = SassFiles.readFiles(),
 			components = [];
 		for (var i = 0; i < files.length; i++) {
-			components = SassComponent.getComponents(files[i]);
+			components = components.concat(SassComponent.getComponents(files[i]));
 			for (var j = 0; j < components.length; j++) {
 				components[j].css = SassParser.parse(components[j].sass + components[j].usage);
 			}
