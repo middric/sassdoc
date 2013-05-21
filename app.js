7bd4f8dcb7d7f64fbcec86417e9c33fb19f4bb6d
middric <richmiddleditch@gmail.com>
2013-05-21 19:24:55 +0100

    adding arguments tag support

--- app.js
+++ app.js
@@ -15,7 +15,6 @@ app.set('views', __dirname + '/views');
 app.engine('.html', require('jade').__express);
 app.set('view engine', 'jade');
 app.set('view options', {layout: false});
-app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
