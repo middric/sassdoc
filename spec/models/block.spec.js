ac8cfd05a9b8490323f7c189ca7388503d7451e4
Rich Middleditch <richmiddleditch@gmail.com>
2013-05-23 10:57:49 +0100

    making the tests DRYer. Utilising beforeeach to remove repitition

--- spec/models/block.spec.js
+++ spec/models/block.spec.js
@@ -3,6 +3,18 @@ var Block = require('../../models/Block.js'),
 	e = require('../../config/exceptions.js');
 
 describe('Block', function () {
+	var file, app, blocks;
+	beforeEach(function () {
+		Files.dir = '';
+		file = Files.readFile('spec/fixtures/sass/styles.scss'),
+		app = require('express')(),
+		blocks = Block.getBlocks(file, app);
+
+		for (var i = blocks.length - 1; i >= 0; i--) {
+			blocks[i].parse();
+		}
+	});
+
 	it("should sort by block type", function () {
 		var blocks = [
 				{isVariable: true},
@@ -19,97 +31,46 @@ describe('Block', function () {
 				{isVariable: false},
 				{isVariable: false},
 				{isVariable: false}
-			]
+			];
 		expect(Block.sort(blocks)).toEqual(expected);
 	});
 
 	it("should return blocks in a file", function () {
-		Files.dir = '';
-		var file = Files.readFile('spec/fixtures/sass/styles.scss'),
-			app = require('express')(),
-			blocks = Block.getBlocks(file, app);
-
 		expect(blocks.length).toBe(3);
 		expect(typeof blocks[0].getID).toBe('function');
 	});
 
 	it("should have detected lines of Sass", function () {
-		Files.dir = '';
-		var file = Files.readFile('spec/fixtures/sass/styles.scss'),
-			app = require('express')(),
-			blocks = Block.getBlocks(file, app);
-
 		expect(blocks[0].getLines().length).toBeTruthy();
 	});
 
 	it("should be able to parse Sass", function () {
-		Files.dir = '';
-		var file = Files.readFile('spec/fixtures/sass/styles.scss'),
-			app = require('express')(),
-			blocks = Block.getBlocks(file, app);
-		blocks[0].parse();
-
 		expect(blocks[0].getID()).toBe('test');
 	});
 
 	it("should detect variables", function () {
-		Files.dir = '';
-		var file = Files.readFile('spec/fixtures/sass/styles.scss'),
-			app = require('express')(),
-			blocks = Block.getBlocks(file, app);
-		blocks[0].parse();
-		blocks[2].parse();
-
 		expect(blocks[0].isVariable).not.toBeTruthy();
 		expect(blocks[2].isVariable).toBeTruthy();
 	});
 
 	it("should contain Sass", function () {
-		Files.dir = '';
-		var file = Files.readFile('spec/fixtures/sass/styles.scss'),
-			app = require('express')(),
-			blocks = Block.getBlocks(file, app);
-		blocks[0].parse();
-
 		expect(blocks[0].getSass()).toBeTruthy();
 	});
 
 	it("should contain markup", function () {
-		Files.dir = '';
-		var file = Files.readFile('spec/fixtures/sass/styles.scss'),
-			app = require('express')(),
-			blocks = Block.getBlocks(file, app);
-		blocks[0].parse();
-
 		expect(blocks[0].getMarkup()).toBeTruthy();
 	});
 
 	it("should be able to retrieve a tag", function () {
-		Files.dir = '';
-		var file = Files.readFile('spec/fixtures/sass/styles.scss'),
-			app = require('express')(),
-			blocks = Block.getBlocks(file, app);
-		blocks[0].parse();
-
 		expect((blocks[0].getTag('name'))[0].getName()).toBe('name');
 		expect((blocks[0].getTag('name'))[0].getValue()).toBe('Test');
 	});
 
 	it("should be able to retrieve a tag value", function () {
-		Files.dir = '';
-		var file = Files.readFile('spec/fixtures/sass/styles.scss'),
-			app = require('express')(),
-			blocks = Block.getBlocks(file, app);
-		blocks[0].parse();
-
 		expect(blocks[0].getTagValue('name')).toBe('Test');
 	});
 
 	it("should list the imports", function () {
-		Files.dir = '';
-		var file = Files.readFile('spec/fixtures/sass/styles.scss'),
-			app = require('express')(),
-			blocks = Block.getBlocks(file, app);
 		app.set('configuration', {
 			useCompass: true,
 			root: '',
@@ -118,39 +79,28 @@ describe('Block', function () {
 				'globalImport'
 			]
 		});
-		blocks[0].parse();
 
 		expect(blocks[0].getImports()).toBe("@import \"compass\";\n" +
-			"@import \"/sass/globalImport\";\n" + 
+			"@import \"/sass/globalImport\";\n" +
 			"@import \"/sass/anotherfile\";");
 	});
 
 	it("should get external CSS dependencies", function () {
-		Files.dir = '';
-		var file = Files.readFile('spec/fixtures/sass/styles.scss'),
-			app = require('express')(),
-			blocks = Block.getBlocks(file, app);
 		app.set('configuration', {
 			externalCSS: [
 				'myCSS.css'
 			]
 		});
-		blocks[0].parse();
 
 		expect(blocks[0].getExternal()).toBe("<link href=\"myCSS.css\" rel=\"stylesheet\" /><link href=\"anothercss\" rel=\"stylesheet\" />");
 	});
 
 	it("should get generated CSS", function () {
-		Files.dir = '';
-		var file = Files.readFile('spec/fixtures/sass/styles.scss'),
-			app = require('express')(),
-			blocks = Block.getBlocks(file, app);
 		app.set('configuration', {
 			useCompass: true,
 			root: '',
 			sassDirectory: 'sass'
 		});
-		blocks[1].parse();
 
 		expect(blocks[1].getCSS()).toBe(
 			".anothertest body {\n" +
--- spec/models/files.spec.js
+++ spec/models/files.spec.js
@@ -2,6 +2,11 @@ var Files = require('../../models/Files.js'),
 	e = require('../../config/exceptions.js');
 
 describe('Files', function () {
+	beforeEach(function () {
+		Files.dir = '';
+		Files.files = null;
+	});
+
 	it("should throw an error if a file cannot be read", function () {
 		expect(function () {
 			Files.readFile('doesntexist');
@@ -9,13 +14,11 @@ describe('Files', function () {
 	});
 
 	it("should return an object if a file can be read", function () {
-		Files.dir = '';
 		var file = Files.readFile('spec/fixtures/sass/styles.scss');
 		expect(file.filename).toBe('spec/fixtures/sass/styles.scss');
 	});
 
 	it("should be able to read a number of files at once", function () {
-		Files.dir = '';
 		Files.files = ['spec/fixtures/sass/styles.scss', 'spec/fixtures/sass/styles.sass'];
 		var files = Files.readFiles();
 
@@ -25,8 +28,6 @@ describe('Files', function () {
 	});
 
 	it("should be able to find and read all of the files in a directory", function () {
-		Files.dir = '';
-		Files.files = null;
 		Files.findFiles('spec/fixtures/sass');
 
 		var files = Files.readFiles();
